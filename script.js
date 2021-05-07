293cb0b51af885200dbc632a510c03b273a0ed6f
osamaHamad0 <>
2021-05-07 16:56:34 +0000

    fix js linter standard

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function checkNegative () {
   // getting the inputted number
   let number = document.getElementById('number').value
 
-  // changing type string to type int 
+  // changing type string to type int
   number = parseInt(number)
 
-  // checking if number is negative 
+  // checking if number is negative
   if (number < 0) {
     // changing result to negative
     document.getElementById('result').innerText = 'your number is negative'
